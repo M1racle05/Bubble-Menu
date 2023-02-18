e57826c12fac5aad742fd3c8ef015f48653dc2b6
Nur Hadi <decoding0506@gmail.com>
2023-02-18 23:30:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function middleBubble() {
+    const middleBub = document.querySelector(".middle-bubble"),
+    splitBubbleLayer = document.querySelector(".bubble-split");
+    middleBub.classList.toggle("active");
+    splitBubbleLayer.classList.toggle("active");
+}
